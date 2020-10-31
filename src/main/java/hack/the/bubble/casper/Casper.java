9eb7a16614f69f2be0402e9fddba9a9a40eb7271
Ryan Delaney <vitineth@gmail.com>
2020-10-31 11:41:11 +0000

    The most basic black window

--- src/main/java/hack/the/bubble/casper/Casper.java
+++ src/main/java/hack/the/bubble/casper/Casper.java
@@ -1,9 +1,12 @@
 package hack.the.bubble.casper;
 
+import de.gurkenlabs.litiengine.Game;
+
 public class Casper {
+
     public static void main(String[] args) {
-
+        Game.init(args);
+        Game.start();
     }
-
 
 }
