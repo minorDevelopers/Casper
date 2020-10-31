cb22a821fd0dbe3bc197faf255a9e78678656269
Ryan Delaney <vitineth@gmail.com>
2020-10-31 17:28:32 +0000

    Creating candyable entities (entities with candys inside)

---
+++ src/main/java/hack/the/bubble/casper/entities/CandyableEntity.java
@@ -0,0 +1,36 @@
+package hack.the.bubble.casper.entities;
+
+import hack.the.bubble.casper.DrawBuffer;
+
+import java.util.Random;
+
+public abstract class CandyableEntity extends BaseEntity {
+
+    protected static final Random random = new Random();
+
+    protected boolean hasCandy;
+
+    public CandyableEntity(DrawBuffer instance, String imageId, int pixWidth, boolean hasCandy) {
+        super(instance, imageId, pixWidth);
+        this.hasCandy = hasCandy;
+    }
+
+    public CandyableEntity(DrawBuffer instance, String imageId, int pixWidth) {
+        this(instance, imageId, pixWidth, random.nextBoolean());
+    }
+
+    public boolean hasCandy() {
+        return hasCandy;
+    }
+
+    public void removeCandy(){
+        hasCandy = false;
+    }
+
+    @Override
+    public void onClicked() {
+        removeCandy();
+        // TODO: increment score
+    }
+
+}
