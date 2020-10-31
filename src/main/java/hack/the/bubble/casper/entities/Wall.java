64e28b6be1d585afadd53d769c86c058e0fbba4d
Ryan Delaney <vitineth@gmail.com>
2020-10-31 17:05:51 +0000

    Adding a wall

---
+++ src/main/java/hack/the/bubble/casper/entities/Wall.java
@@ -0,0 +1,39 @@
+package hack.the.bubble.casper.entities;
+
+import hack.the.bubble.casper.DrawBuffer;
+
+import java.awt.Rectangle;
+
+public class Wall extends BaseEntity{
+
+    private int width;
+    private int height;
+
+    public Wall(DrawBuffer instance, int width, int height) {
+        super(instance, null, 0);
+        this.width = width;
+        this.height = height;
+        this.setEntityType("wall.solid");
+    }
+
+    @Override
+    public void draw() {
+        this.mainInstance.rect(this.getPosX(), this.getPosY(), width, height);
+    }
+
+    @Override
+    public Rectangle hitbox() {
+        return new Rectangle(getPosX(), getPosY(), width, height);
+    }
+
+    @Override
+    public void onCollide(BaseEntity e) {
+        System.out.println("Coll?");
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+}
