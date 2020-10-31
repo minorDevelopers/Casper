ae3d61bd77b573efdb020f7c5e2df03b9ab12176
Ryan Delaney <vitineth@gmail.com>
2020-10-31 15:31:58 +0000

    Migrating to entity rendering lists

--- src/main/java/hack/the/bubble/casper/Casper.java
+++ src/main/java/hack/the/bubble/casper/Casper.java
@@ -1,24 +1,38 @@
 package hack.the.bubble.casper;
 
+import hack.the.bubble.casper.entities.BaseEntity;
+import hack.the.bubble.casper.entities.Candy;
 import hack.the.bubble.casper.entities.Player;
 import hack.the.bubble.casper.interaction.KeyManager;
 import processing.core.PApplet;
 import processing.event.KeyEvent;
+import processing.event.MouseEvent;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class Casper extends PApplet {
 
     private KeyManager manager = new KeyManager();
     private Player player;
     private Player player2;
+    private Collection<BaseEntity> entities = new ArrayList<>();
 
 
     @Override
     public void settings() {
         this.player = new Player(this, "player-male");
         this.player2 = new Player(this, "player-female");
-        size(720, 480);
+
+        size(1900, 1030);
+
+        entities.add(player);
+        entities.add(player2);
+
+        for (int i = 0; i < 40; i++) {
+            entities.add(new Candy(this));
+        }
     }
 
     @Override
@@ -27,16 +41,17 @@ public class Casper extends PApplet {
     }
 
     @Override
+    protected void handleMouseEvent(MouseEvent event) {
+        super.handleMouseEvent(event);
+    }
+
+    @Override
     public void draw() {
         int bc = 0xff0ff;
-        if(player.intersects(player2))  bc = 0xff0000;
+        if (player.intersects(player2)) bc = 0xff0000;
         background(bc);
-        text("Hello, World!", width / 2, height / 2);
 
-        // Draw each entity
-        player.draw();
-        player2.draw();
-
+        this.entities.forEach(BaseEntity::draw);
 
         if (manager.isPressed('w')) {
             player.move("up");
@@ -58,6 +73,9 @@ public class Casper extends PApplet {
             ResourceManager.getInstance().registerSprite("player-vampire", Casper.class.getResource("/Vampire.png"));
             ResourceManager.getInstance().registerSprite("player-male", Casper.class.getResource("/Player_Male.png"));
             ResourceManager.getInstance().registerSprite("player-female", Casper.class.getResource("/Player_Female.png"));
+            ResourceManager.getInstance().registerSprite("candy.corn", Casper.class.getResource("/candy/CandyCorn.png"));
+            ResourceManager.getInstance().registerSprite("candy.lolly", Casper.class.getResource("/candy/Lollipop.png"));
+            ResourceManager.getInstance().registerSprite("candy.wrapped", Casper.class.getResource("/Candy/WrappedCandy.png"));
             ResourceManager.getInstance().registerSpriteSheet("floor-indoor", Casper.class.getResource("/IndoorFloor.png"), 1024, 1024, 0, 0);
         } catch (IOException e) {
             e.printStackTrace();
