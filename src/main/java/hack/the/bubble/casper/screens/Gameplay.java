ba6799b7074e446d23aa7dccb1d02f535cc4dc27
Mika <mikaela.schmeling@gmail.com>
2020-10-31 18:03:42 +0000

    New Package to do screens - beginning

---
+++ src/main/java/hack/the/bubble/casper/screens/Gameplay.java
@@ -0,0 +1,10 @@
+package hack.the.bubble.casper.screens;
+
+import hack.the.bubble.casper.entities.BaseEntity;
+import hack.the.bubble.casper.entities.Player;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Gameplay {
+}
