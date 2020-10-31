afe6fc5e9b30e6f94821c8571b153ba205718f29
Ryan Delaney <vitineth@gmail.com>
2020-10-31 15:31:22 +0000

    Adding basic candy entity

---
+++ src/main/java/hack/the/bubble/casper/entities/Candy.java
@@ -0,0 +1,50 @@
+package hack.the.bubble.casper.entities;
+
+import hack.the.bubble.casper.ResourceManager;
+import processing.core.PApplet;
+
+import java.util.Random;
+
+public class Candy extends BaseEntity {
+
+    private static final String[] IMAGE_OPTIONS = {
+            "candy.corn",
+            "candy.lolly",
+            "candy.wrapped",
+    };
+    private static final Random random = new Random();
+
+    private final String sprite;
+    private boolean isVisible = true;
+
+    public Candy(PApplet applet) {
+        super(applet);
+        this.sprite = IMAGE_OPTIONS[random.nextInt(IMAGE_OPTIONS.length)];
+
+        // TODO: generate positions based on map
+        this.setPosX(random.nextInt(applet.width));
+        this.setPosY(random.nextInt(applet.height));
+    }
+
+    @Override
+    public void draw() {
+        if (!isVisible) return;
+        this.mainInstance.image(
+                ResourceManager.getInstance().getImage(sprite),
+                this.getPosX(),
+                this.getPosY(),
+                50,
+                ResourceManager.getInstance().getScaledHeight(this.sprite, 50)
+        );
+    }
+
+    @Override
+    public void update() {
+
+    }
+
+    @Override
+    public void onCollide() {
+        isVisible = false;
+    }
+}
